efbe122d575b069fe818177e2aece8d7e3a9a127
Karl August Nestor <jesuscaesar@outlook.com>
2022-08-23 04:49:50 +1000

    Added new files

--- oncompetitor.civilization.parfunc.php
+++ oncompetitor.civilization.parfunc.php
@@ -1,6 +1,6 @@
 <?php
 
-$subActions = ["pass"];
+$subActions = ["pass", "agent"];
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 if ($subAction == "pass") {
@@ -15,4 +15,104 @@ if ($subAction == "pass") {
         "] " .
         $spacedictus[$proLingo]["pass"] .
         "<br>";
+} elseif ($subAction == "agent") {
+    $subOpsList = rand('navigate', 'attack');
+    $subOpsCount = count($subOpsList);
+    $subOper = $subOpsList[rand(0, $subOpsCount - 1)];
+    if ($subOper == 'navigate') {
+        $subDirect = rand(0, 3);
+        if ($subDirect == 0) {
+            $subX += $subMove;
+            $subScore += 0.1;
+            echo $turnNum .
+                " : " .
+                $subModeSign .
+                $sub .
+                "[" .
+                $subRating .
+                "] " .
+                $spacedictus[$proLingo]["right"] .
+                " {" .
+                $subX .
+                ";" .
+                $subY .
+                ";" .
+                $subZ .
+                "}<br>";
+        } elseif ($subDirect == 1) {
+            $subX -= $subMove;
+            $subScore += 0.1;
+            echo $turnNum .
+                " : " .
+                $subModeSign .
+                $sub .
+                "[" .
+                $subRating .
+                "] " .
+                $spacedictus[$proLingo]["left"] .
+                " {" .
+                $subX .
+                ";" .
+                $subY .
+                ";" .
+                $subZ .
+                "}<br>";
+        } elseif ($subDirect == 2) {
+            $subY += $subMove;
+            $subScore += 0.1;
+            echo $turnNum .
+                " : " .
+                $subModeSign .
+                $sub .
+                "[" .
+                $subRating .
+                "] " .
+                $spacedictus[$proLingo]["forward"] .
+                " {" .
+                $subX .
+                ";" .
+                $subY .
+                ";" .
+                $subZ .
+                "}<br>";
+        } elseif ($subDirect == 3) {
+            $subY -= $subMove;
+            $subScore += 0.1;
+            echo $turnNum .
+                " : " .
+                $subModeSign .
+                $sub .
+                "[" .
+                $subRating .
+                "] " .
+                $spacedictus[$proLingo]["back"] .
+                " {" .
+                $subX .
+                ";" .
+                $subY .
+                ";" .
+                $subZ .
+                "}<br>";
+        }
+    } elseif ($subOper == 'attack') {
+        $objRating -= $subForce + $objShield;
+        $subRating += $subForce - $objShield;
+        $subScore += 5;
+        echo $turnNum .
+            " : " .
+            $subModeSign .
+            $sub .
+            "[" .
+            $subRating .
+            "] " .
+            $subWeaponName .
+            " (" .
+            $subForce .
+            ") " .
+            $objModeSign .
+            $obj .
+            "[" .
+            $objRating .
+            "]<br>";
+    }
 }
