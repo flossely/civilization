913dceadeeff06533cf72b0404b714684c18956c
Karl August Nestor <jesuscaesar@outlook.com>
2022-08-23 04:48:22 +1000

    Added new files

--- onbeneficiary.civilization.parfunc.php
+++ onbeneficiary.civilization.parfunc.php
@@ -4,6 +4,23 @@ $subActions = ["envoy", "pass"];
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 if ($subAction == "envoy") {
+    file_put_contents($obj.'/'.$sub.'.envoy.obj', 'suzerain|[>]|'.$sub.'|[1]|vassal|[>]|'.$obj.'|[1]|relations|[>]|Envoy');
+    chmod($obj.'/'.$sub.'.envoy.obj', 0777);
+    $subRating += 0.05;
+    $subScore += 10;
+    echo $turnNum .
+        " : " .
+        $subModeSign .
+        $sub .
+        "[" .
+        $subRating .
+        "] " .
+        $spacedictus[$proLingo]["envoy"] .
+        $objModeSign .
+        $obj .
+        "[" .
+        $objRating .
+        "<br>";
 } elseif ($subAction == "pass") {
     $subRating += 0.01;
     $subScore += 1;
