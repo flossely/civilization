7fff3b0e01d7248e379b088343dcc9764d13320e
Karl August Nestor <jesuscaesar@outlook.com>
2022-08-24 04:22:44 +1000

    Added new files

--- oncompetitor.civilization.parfunc.php
+++ oncompetitor.civilization.parfunc.php
@@ -1,8 +1,13 @@
 <?php
 
-$subActions = ["pass", "explore", "actions"];
+$subActions =
+[
+    'pass', 'explore', 'mine', 'treasure', 'buy', 'sell'
+];
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
+
+include 'mine.civilization.parfunc.php';
 
 if ($subAction == "pass") {
     $subRating += 0.01;
@@ -15,173 +20,88 @@ if ($subAction == "pass") {
     $subZ = $msgBox['z'];
     $subRating += 0.01;
     $subScore += 1;
-} elseif ($subAction == "actions") {
-    if ($subRearmed) {
-        if ($subVehicleType == 'land') {
-            $subWhoActions = ["drive", "shoot"];
-            $subWhoActionCount = count($subWhoActions);
-            $subWhoAction = $subWhoActions[rand(0, $subWhoActionCount - 1)];
-            if (!classEmpty($sub, 'vehicle')) {
-                $subWhoActions[] = 'exit';
-            }
-            if ($subOper == 'drive') {
-                $msgBox = movement($turnNum, $subHalfNotation, $subX, $subY, $subZ, 3, $subVehicleSpeed);
-                $subX = $msgBox['x'];
-                $subY = $msgBox['y'];
-                $subZ = $msgBox['z'];
-            } elseif ($subOper == 'shoot') {
-                $objRating -= $subVehicleDamage + $objShield;
-                $subRating += $subVehicleDamage - $objShield;
-                $subScore += 10;
-                echo $turnNum." : ".$subHalfNotation.' '.$subVehicleName." (".$subVehicleDamage."/".$objShield.") ".$objHalfNotation."<br>";
-            } elseif ($subOper == 'exit') {
-                $subRearmed = false;
-            }
-        } elseif ($subVehicleType == 'naval') {
-            $subWhoActions = ["seal", "shoot"];
-            $subWhoActionCount = count($subWhoActions);
-            $subWhoAction = $subWhoActions[rand(0, $subWhoActionCount - 1)];
-            if (!classEmpty($sub, 'vehicle')) {
-                $subWhoActions[] = 'exit';
-            }
-            if ($subOper == 'seal') {
-                $msgBox = movement($turnNum, $subHalfNotation, $subX, $subY, $subZ, 3, $subVehicleSpeed);
-                $subX = $msgBox['x'];
-                $subY = $msgBox['y'];
-                $subZ = $msgBox['z'];
-            } elseif ($subOper == 'shoot') {
-                $objRating -= $subVehicleDamage + $objShield;
-                $subRating += $subVehicleDamage - $objShield;
-                $subScore += 10;
-                echo $turnNum." : ".$subHalfNotation.' '.$subVehicleName." (".$subVehicleDamage."/".$objShield.") ".$objHalfNotation."<br>";
-            } elseif ($subOper == 'exit') {
-                $subRearmed = false;
-            }
-        } elseif ($subVehicleType == 'aircraft') {
-            $subWhoActions = ["flight", "shoot"];
-            $subWhoActionCount = count($subWhoActions);
-            $subWhoAction = $subWhoActions[rand(0, $subWhoActionCount - 1)];
-            if (!classEmpty($sub, 'vehicle')) {
-                $subWhoActions[] = 'exit';
-            }
-            if ($subOper == 'flight') {
-                $msgBox = movement($turnNum, $subHalfNotation, $subX, $subY, $subZ, 5, $subVehicleSpeed);
-                $subX = $msgBox['x'];
-                $subY = $msgBox['y'];
-                $subZ = $msgBox['z'];
-            } elseif ($subOper == 'shoot') {
-                $objRating -= $subVehicleDamage + $objShield;
-                $subRating += $subVehicleDamage - $objShield;
-                $subScore += 10;
-                echo $turnNum." : ".$subHalfNotation.' '.$subVehicleName." (".$subVehicleDamage."/".$objShield.") ".$objHalfNotation."<br>";
-            } elseif ($subOper == 'exit') {
-                $subRearmed = false;
-            }
-        }
+} elseif ($subAction == "mine") {
+    $subRes = ['gold', 'uranium'];
+    $subResCount = count($subRes);
+    $subMine = $subRes[rand(0, $subResCount - 1)];
+    if ($subMine == 'gold') {
+        $subGold += 1;
+        echo $turnNum." : ".$subNotation." ".$spacedictus[$proLingo]["mine"]." 1 ".$spacedictus[$proLingo]["gold"]."<br>";
+        file_put_contents($sub.'/gold', $subGold);
+        chmod($sub.'/gold', 0777);
+    } elseif ($subMine == 'uranium') {
+        $subUranium += 1;
+        echo $turnNum." : ".$subNotation." ".$spacedictus[$proLingo]["mine"]." 1 ".$spacedictus[$proLingo]["uranium"]."<br>";
+        file_put_contents($sub.'/uranium', $subUranium);
+        chmod($sub.'/uranium', 0777);
+    }
+} elseif ($subAction == "treasure") {
+    if ($subGold > 0) {
+        $subMoney += $subMoney + $subScore * $subGold;
+        echo $turnNum." : ".$subHalfNotation.' '.$spacedictus[$proLingo]["treasure"]."<br>";
     } else {
-        $subWhoActions = ["agent", "soldier", "knight", "trade"];
-        $subWhoActionCount = count($subWhoActions);
-        $subWhoAction = $subWhoActions[rand(0, $subWhoActionCount - 1)];
-        if (!classEmpty($sub, 'vehicle')) {
-            $subWhoActions[] = 'enter';
-        }
-        if ($subWhoAction == "agent") {
-            $subOpsList = ['navigate', 'melee'];
-            $subOpsCount = count($subOpsList);
-            $subOper = $subOpsList[rand(0, $subOpsCount - 1)];
-            if ($subOper == 'navigate') {
-                $msgBox = movement($turnNum, $subHalfNotation, $subX, $subY, $subZ, 3, $subMove);
-                $subX = $msgBox['x'];
-                $subY = $msgBox['y'];
-                $subZ = $msgBox['z'];
-            } elseif ($subOper == 'melee') {
-                $objRating -= $subMeleeForce;
-                $subRating += $subMeleeForce;
-                $subScore += 20;
-                echo $turnNum." : ".$subHalfNotation.' '.$subForceType." (".$subMeleeForce."/".$objShield.") ".$objHalfNotation."<br>";
-            }
-        } elseif ($subWhoAction == "knight") {
-            $subOpsList = ['navigate', 'melee'];
-            $subOpsCount = count($subOpsList);
-            $subOper = $subOpsList[rand(0, $subOpsCount - 1)];
-            if ($subOper == 'navigate') {
-                $msgBox = movement($turnNum, $subHalfNotation, $subX, $subY, $subZ, 3, $subMove);
-                $subX = $msgBox['x'];
-                $subY = $msgBox['y'];
-                $subZ = $msgBox['z'];
-            } elseif ($subOper == 'melee') {
-                $objRating -= $subMeleeForce;
-                $subRating += $subMeleeForce;
-                $subScore += 20;
-                echo $turnNum." : ".$subHalfNotation.' '.$subForceType." (".$subMeleeForce."/".$objShield.") ".$objHalfNotation."<br>";
-            }
-        } elseif ($subWhoAction == "soldier") {
-            $subOpsList = ['navigate', 'assault'];
-            $subOpsCount = count($subOpsList);
-            $subOper = $subOpsList[rand(0, $subOpsCount - 1)];
-            if ($subOper == 'navigate') {
-                $msgBox = movement($turnNum, $subHalfNotation, $subX, $subY, $subZ, 3, $subMove);
-                $subX = $msgBox['x'];
-                $subY = $msgBox['y'];
-                $subZ = $msgBox['z'];
-            } elseif ($subOper == 'assault') {
-                $objRating -= $subForce + $objShield;
-                $subRating += $subForce - $objShield;
-                $subScore += 10;
-                echo $turnNum." : ".$subHalfNotation.' '.$subForceType." (".$subForce."/".$objShield.") ".$objHalfNotation."<br>";
-            }
-        } elseif ($subWhoAction == "enter") {
-            $subRearmed = true;
-        } elseif ($subWhoAction == "trade") {
-            $subWantsTrade = ['item', 'weapon', 'melee', 'vehicle'];
-            $subWantsCount = count($subWantsTrade);
-            $subWantAction = $subWantsTrade[rand(0, $subWantsCount - 1)];
-    
-            if ($subWantAction == 'item') {
-                $subTradeMode = rand(0, 1);
-                if ($subTradeMode == 0) {
-                    $msgBox = initExchange($thisParadigm, $yearToday, $obj, $sub, $objMoney, $subMoney, ratioCalc($objEconVal, $subEconVal), $objUseItem);
-                    $objMoney = $msgBox['debit'];
-                    $subMoney = $msgBox['credit'];
-                } elseif ($subTradeMode == 1) {
-                    $msgBox = initExchange($thisParadigm, $yearToday, $sub, $obj, $subMoney, $objMoney, ratioCalc($subEconVal, $objEconVal), $subUseItem);
-                    $subMoney = $msgBox['debit'];
-                    $objMoney = $msgBox['credit'];
-                }
-            } elseif ($subWantAction == 'weapon') {
-                $subTradeMode = rand(0, 1);
-                if ($subTradeMode == 0) {
-                    $msgBox = initExchange($thisParadigm, $yearToday, $obj, $sub, $objMoney, $subMoney, ratioCalc($objEconVal, $subEconVal), $objUseWeapon);
-                    $objMoney = $msgBox['debit'];
-                    $subMoney = $msgBox['credit'];
-                } elseif ($subTradeMode == 1) {
-                    $msgBox = initExchange($thisParadigm, $yearToday, $sub, $obj, $subMoney, $objMoney, ratioCalc($subEconVal, $objEconVal), $subUseWeapon);
-                    $subMoney = $msgBox['debit'];
-                    $objMoney = $msgBox['credit'];
-                }
-            } elseif ($subWantAction == 'melee') {
-                $subTradeMode = rand(0, 1);
-                if ($subTradeMode == 0) {
-                    $msgBox = initExchange($thisParadigm, $yearToday, $obj, $sub, $objMoney, $subMoney, ratioCalc($objEconVal, $subEconVal), $objUseMelee);
-                    $objMoney = $msgBox['debit'];
-                    $subMoney = $msgBox['credit'];
-                } elseif ($subTradeMode == 1) {
-                    $msgBox = initExchange($thisParadigm, $yearToday, $sub, $obj, $subMoney, $objMoney, ratioCalc($subEconVal, $objEconVal), $subUseMelee);
-                    $subMoney = $msgBox['debit'];
-                    $objMoney = $msgBox['credit'];
-                }
-            } elseif ($subWantAction == 'vehicle') {
-                $subTradeMode = rand(0, 1);
-                if ($subTradeMode == 0) {
-                    $msgBox = initExchange($thisParadigm, $yearToday, $obj, $sub, $objMoney, $subMoney, ratioCalc($objEconVal, $subEconVal), $objUseVehicle);
-                    $objMoney = $msgBox['debit'];
-                    $subMoney = $msgBox['credit'];
-                } elseif ($subTradeMode == 1) {
-                    $msgBox = initExchange($thisParadigm, $yearToday, $sub, $obj, $subMoney, $objMoney, ratioCalc($subEconVal, $objEconVal), $subUseVehicle);
-                    $subMoney = $msgBox['debit'];
-                    $objMoney = $msgBox['credit'];
-                }
-            }
-        }
+        $subRating += 0.01;
+        $subScore += 1;
+        echo $turnNum." : ".$subHalfNotation.' '.$spacedictus[$proLingo]["pass"]."<br>";
+    }
+} elseif ($subAction == 'buy') {
+    $msgBox = initExchange($thisParadigm, $yearToday, $obj, $sub, $objMoney, $subMoney, ratioCalc($objEconVal, $subEconVal), $objUseItem);
+    $objMoney = $msgBox['debit'];
+    $subMoney = $msgBox['credit'];
+} elseif ($subAction == 'sell') {
+    $msgBox = initExchange($thisParadigm, $yearToday, $sub, $obj, $subMoney, $objMoney, ratioCalc($subEconVal, $objEconVal), $subUseItem);
+    $subMoney = $msgBox['debit'];
+    $objMoney = $msgBox['credit'];
+} elseif ($subAction == 'make_nuke') {
+    if ($subUranium > 0) {
+        $subNukes += 1;
+        $subScore += 10;
+        echo $turnNum." : ".$subHalfNotation.' '.$spacedictus[$proLingo]["produce"].' '.$spacedictus[$proLingo]["nuke"]."<br>";
+        file_put_contents($sub.'/nukes', $subNukes);
+        chmod($sub.'/nukes', 0777);
+    } else {
+        $subRating += 0.01;
+        $subScore += 1;
+        echo $turnNum." : ".$subHalfNotation.' '.$spacedictus[$proLingo]["pass"]."<br>";
+    }
+} elseif ($subAction == 'make_thermonuke') {
+    if ($subUranium > 0) {
+        $subThermoNukes += 1;
+        $subScore += 20;
+        echo $turnNum." : ".$subHalfNotation.' '.$spacedictus[$proLingo]["produce"].' '.$spacedictus[$proLingo]["nuke"]."<br>";
+        file_put_contents($sub.'/thermonukes', $subThermoNukes);
+        chmod($sub.'/thermonukes', 0777);
+    } else {
+        $subRating += 0.01;
+        $subScore += 1;
+        echo $turnNum." : ".$subHalfNotation.' '.$spacedictus[$proLingo]["pass"]."<br>";
+    }
+} elseif ($subAction == "nuke") {
+    if ($subNukes > 0) {
+        $objRating -= $subForce * 50;
+        $subRating += $subForce * 5;
+        $subNukes -= 1;
+        $subScore += 50;
+        echo $turnNum." : ".$subHalfNotation.' '.$spacedictus[$proLingo]["nuke"].' '.$objHalfNotation."<br>";
+        file_put_contents($sub.'/nukes', $subNukes);
+        chmod($sub.'/nukes', 0777);
+    } else {
+        $subRating += 0.01;
+        $subScore += 1;
+        echo $turnNum." : ".$subHalfNotation.' '.$spacedictus[$proLingo]["pass"]."<br>";
+    }
+} elseif ($subAction == "thermonuke") {
+    if ($subThermoNukes > 0) {
+        $objRating -= $subForce * 100;
+        $subRating += $subForce * 10;
+        $subThermoNukes -= 1;
+        $subScore += 100;
+        echo $turnNum." : ".$subHalfNotation.' '.$spacedictus[$proLingo]["nuke"].' '.$objHalfNotation."<br>";
+        file_put_contents($sub.'/thermonukes', $subThermoNukes);
+        chmod($sub.'/thermonukes', 0777);
+    } else {
+        $subRating += 0.01;
+        $subScore += 1;
+        echo $turnNum." : ".$subHalfNotation.' '.$spacedictus[$proLingo]["pass"]."<br>";
     }
 }
