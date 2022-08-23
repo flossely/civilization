6d689adb5c094e267ee5205a65e44950b46e8c1b
Karl August Nestor <jesuscaesar@outlook.com>
2022-08-24 04:25:00 +1000

    Added new files

--- oncompetitor.civilization.parfunc.php
+++ oncompetitor.civilization.parfunc.php
@@ -7,7 +7,30 @@ $subActions =
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 
-include 'mine.civilization.parfunc.php';
+if (file_exists($sub.'/gold')) {
+    $subGold = file_get_contents($sub.'/gold');
+} else {
+    $subGold = 0;
+}
+if (file_exists($sub.'/uranium')) {
+    $subUraniun = file_get_contents($sub.'/uranium');
+    $subActions[] = "make_nuke";
+    $subActions[] = "make_thermonuke";
+} else {
+    $subUranium = 0;
+}
+if (file_exists($sub.'/nukes')) {
+    $subNukes = file_get_contents($sub.'/nukes');
+    $subActions[] = "nuke";
+} else {
+    $subNukes = 0;
+}
+if (file_exists($sub.'/thermonukes')) {
+    $subThermoNukes = file_get_contents($sub.'/thermonukes');
+    $subActions[] = "thermonuke";
+} else {
+    $subThermoNukes = 0;
+}
 
 if ($subAction == "pass") {
     $subRating += 0.01;
