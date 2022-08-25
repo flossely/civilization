467883a3fd5e30427703da63ba46d1235b153cbf
Karl August Nestor <jesuscaesar@outlook.com>
2022-08-26 07:59:28 +1000

    Added new files

--- onrc.civilization.parfunc.php
+++ onrc.civilization.parfunc.php
@@ -1,15 +1,15 @@
 <?php
 
-$subActions = ["buy", "shield"];
+$subActions = ["weapon", "bomb"];
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 
-if ($subAction == "buy") {
+if ($subAction == "weapon") {
     $msgBox = initExchange($thisParadigm, $yearToday, $obj, $sub, $objMoney, $subMoney, ratioCalc($objEconVal, $subEconVal), $objUseWeapon);
     $objMoney = $msgBox['debit'];
     $subMoney = $msgBox['credit'];
-} elseif ($subAction == "shield") {
-    $msgBox = initExchange($thisParadigm, $yearToday, $obj, $sub, $objMoney, $subMoney, ratioCalc($objEconVal, $subEconVal), $objUseShield);
+} elseif ($subAction == "bomb") {
+    $msgBox = initExchange($thisParadigm, $yearToday, $obj, $sub, $objMoney, $subMoney, ratioCalc($objEconVal, $subEconVal), $objUseBomb);
     $objMoney = $msgBox['debit'];
     $subMoney = $msgBox['credit'];
 }
--- onrl.civilization.parfunc.php
+++ onrl.civilization.parfunc.php
@@ -1,33 +1,31 @@
 <?php
 
-$subActions = ['assault'];
+$subActions = ['assault', 'bombard'];
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
-
-if ($subUseBomb !== null) {
-    $subActions[] = 'bombard';
-}
 
 if ($subAction == "assault") {
     if ($objUseShield !== null) {
         $objRating -= $subForce + $objShield;
         $subRating += $subForce - $objShield;
         $subScore += $subForce - $objShield;
+        echo $turnNum." : ".$subFullName.' '.$subForceType." (".$subForce."/".$objShield.") ".$objFullName."<br>";
     } else {
         $objRating -= $subForce;
         $subRating += $subForce;
         $subScore += $subForce;
+        echo $turnNum." : ".$subFullName.' '.$subForceType." (".$subForce.") ".$objFullName."<br>";
     }
-    echo $turnNum." : ".$subFullName.' '.$subForceType." (".$subForce."/".$objShield.") ".$objFullName."<br>";
 } elseif ($subAction == "bombard") {
     if ($objUseShield !== null) {
         $objRating -= $subBombForce + $objShield;
         $subRating += $subBombForce - $objShield;
         $subScore += $subBombForce - $objShield;
+        echo $turnNum." : ".$subFullName.' '.$subBombType." (".$subBombForce."/".$objShield.") ".$objFullName."<br>";
     } else {
         $objRating -= $subBombForce;
         $subRating += $subBombForce;
         $subScore += $subBombForce;
+        echo $turnNum." : ".$subFullName.' '.$subBombType." (".$subBombForce.") ".$objFullName."<br>";
     }
-    echo $turnNum." : ".$subFullName.' '.$subBombType." (".$subBombForce."/".$objShield.") ".$objFullName."<br>";
 }
