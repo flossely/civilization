a99e4120ab8a917a1d3f5b19635d4f1d3f8e3c1f
Karl August Nestor <jesuscaesar@outlook.com>
2022-10-06 01:41:02 +0000

    Latest update

--- civilization.bsrp.php
+++ civilization.bsrp.php
@@ -114,62 +114,10 @@ if (file_exists('mode')) {
     $mode = $paradigmData['default_mode'];
 }
 
-$civiductus = [];
-include $lingua.'.cividictus.php';
-
-function yearconv($year)
-{
-    if ($year >= 0) {
-        $append = 'AD';
-        $num = $year;
-    } else {
-        $append = 'BC';
-        $num = abs($year);
-    }
-    return $num . ' ' . $append;
-}
-function erayear($year)
-{
-    if ($year < -1000) {
-        $getEra = 'i';
-    } elseif (($year >= -1000) && ($year < 476)) {
-        $getEra = 'ii';
-    } elseif (($year >= 476) && ($year < 1500)) {
-        $getEra = 'iii';
-    } elseif (($year >= 1500) && ($year < 1700)) {
-        $getEra = 'iv';
-    } elseif (($year >= 1700) && ($year < 1900)) {
-        $getEra = 'v';
-    } elseif (($year >= 1900) && ($year < 1950)) {
-        $getEra = 'vi';
-    } elseif (($year >= 1950) && ($year < 1990)) {
-        $getEra = 'vii';
-    } elseif (($year >= 1990) && ($year < 2050)) {
-        $getEra = 'viii';
-    } elseif (($year >= 2050) && ($year < 2100)) {
-        $getEra = 'ix';
-    } elseif ($year >= 2100) {
-        $getEra = 'x';
-    }
-    return $getEra;
-}
-
-include 'civconst.php';
-include 'civeramap.php';
-
-$civ = [];
 $add = $_REQUEST['id'];
-$era = erayear($today);
 $data = $_REQUEST['data'];
 
 $meta = parseGetData($data);
-
-gitPerform('https://github.com', $add.'-civ', '', 'civhub', $add.'.civ.php', $add, $add.'.civ.php');
-
-include $add.'.civ.php';
-if (!array_key_exists($era, $civ[$add]['var'])) {
-    $era = array_key_first($civ[$add]['var']);
-}
 
 if (!file_exists($add)) {
     mkdir($add);
@@ -177,11 +125,11 @@ if (!file_exists($add)) {
 }
 
 if (!file_exists($add.'/coord')) {
-    file_put_contents($add.'/coord', $civ[$add]['coord']);
+    file_put_contents($add.'/coord', $paradigmData['default_coord']);
     chmod($add.'/coord', 0777);
 }
 if (!file_exists($add.'/rating')) {
-    file_put_contents($add.'/rating', $civeramap[$era]['rating']);
+    file_put_contents($add.'/rating', $paradigmData['default_rating']);
     chmod($add.'/rating', 0777);
 }
 file_put_contents($add.'/mode', $mode);
@@ -202,62 +150,24 @@ file_put_contents($add.'/locale', $lingua);
 chmod($add.'/locale', 0777);
 intoZone($add);
 
-if (!file_exists($add.'/home.coord')) {
-    file_put_contents($add.'/home.coord', $civ[$add]['coord']);
-    chmod($add.'/home.coord', 0777);
-}
-file_put_contents($add.'/settle.coord', $civ[$add]['coord']);
-chmod($add.'/settle.coord', 0777);
-
-file_put_contents($add.'/name', $civ[$add]['var'][$era]['name'][$lingua]);
-chmod($add.'/name', 0777);
-file_put_contents($add.'/leader', $civ[$add]['var'][$era]['leader'][$lingua]);
-chmod($add.'/leader', 0777);
-file_put_contents($add.'/era', $era);
-chmod($add.'/era', 0777);
-file_put_contents($add.'/economy', $civ[$add]['var'][$era]['economy']);
-chmod($add.'/economy', 0777);
-file_put_contents($add.'/government', $civ[$add]['var'][$era]['government']);
-chmod($add.'/government', 0777);
-
-if (isset($civ[$add]['var'][$era]['title'])) {
-    file_put_contents($add.'/title', $civ[$add]['var'][$era]['title']);
-    chmod($add.'/title', 0777);
+if (isset($meta['name'])) {
+    file_put_contents($add.'/name', $meta['name']);
+    chmod($add.'/name', 0777);
 }
 
-$startyear = $civeramap[$era]['started'];
-$endyear = $civeramap[$era]['ended'];
-if ($startyear == INFINITY_BC) {
-    $yrex = $startyear;
-    $yrad = yearconv($endyear);
-} elseif ($endyear == INFINITY_AD) {
-    $yrex = yearconv($startyear);
-    $yrad = $endyear;
-} else {
-    $yrex = yearconv($startyear);
-    $yrad = yearconv($endyear);
+if (isset($meta['leader'])) {
+    file_put_contents($add.'/leader', $meta['leader']);
+    chmod($add.'/leader', 0777);
 }
-$erainfo = $civeramap[$era]['era'] . ' (' . $yrex . ' - ' . $yrad . ')';
-file_put_contents($add.'/erainfo.txt', $erainfo);
-chmod($add.'/erainfo.txt', 0777);
 
-if (isset($civ[$add]['var'][$era]['title'])) {
-    if ($civ[$add]['var'][$era]['government'] == DEMOCRACY || $civ[$add]['var'][$era]['government'] == FASCISM || $civ[$add]['var'][$era]['government'] == COMMUNISM) {
-        $civbard = $civ[$add]['var'][$era]['title'] . ' ' . $cividictus[$lingua]['de'] . ' ' . $civ[$add]['var'][$era]['name'][$lingua] . ', ' . $civ[$add]['var'][$era]['leader'][$lingua];
+if (isset($meta['item'])) {
+    gitPerform('https://github.com', 'thingy', 'main', 'wholemarket', $meta['item'].'.item.obj', $add, $meta['item'].'.item.obj');
+}
+
+if (isset($meta['civ'])) {
+    if (isset($meta['era'])) {
+        gitPerform('https://github.com', $meta['civ'], '', 'civhub', $meta['era'].'.png', $add, 'favicon.png');
     } else {
-        $civbard = $civ[$add]['var'][$era]['title'] . ' ' . $civ[$add]['var'][$era]['leader'][$lingua] . ' ' . $cividictus[$lingua]['de'] . ' ' . $civ[$add]['var'][$era]['name'][$lingua];
+        gitPerform('https://github.com', $meta['civ'], '', 'civhub', 'favicon.png', $add, 'favicon.png');
     }
-} else {
-    $civbard = $civ[$add]['var'][$era]['leader'][$lingua] . ' ' . $cividictus[$lingua]['de'] . ' ' . $civ[$add]['var'][$era]['name'][$lingua];
-}
-$civinfo = $civbard . ' (' . $civ[$add]['var'][$era]['economy'] . ' ' . $civ[$add]['var'][$era]['government'] . ')';
-file_put_contents($add.'/civinfo.txt', $civinfo);
-chmod($add.'/civinfo.txt', 0777);
-
-gitPerform('https://github.com', $add.'-ico', 'main', 'civhub', 'era-'.$era.'.png', $add, 'favicon.png');
-if (isset($meta['weapon'])) {
-    gitPerform('https://github.com', 'equipment', $paradigm, 'wholemarket', $meta['weapon'].'.weapon.obj', $add, $meta['weapon'].'.weapon.obj');
-}
-if (isset($meta['shield'])) {
-    gitPerform('https://github.com', 'equipment', $paradigm, 'wholemarket', $meta['shield'].'.shield.obj', $add, $meta['shield'].'.shield.obj');
 }
