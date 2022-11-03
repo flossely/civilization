1f6fdce78bcdaa231ac26d515b45b11edffbeed0
Karl August Nestor <jesuscaesar@outlook.com>
2022-11-03 00:24:37 +0000

    Improved functionality

--- onrr.civilization.parfunc.php
+++ onrr.civilization.parfunc.php
@@ -1,11 +1,20 @@
 <?php
 
-$subActions = ["pass"];
+$subActions = ["pass", "produce"];
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 
 if ($subAction == "pass") {
-    $subRating += 0.1;
-    $subScore += 0.1;
+    $subRating += 1;
+    $subMoney += 1;
+    $subScore += 1;
     echo $turnNum." : ".$subFullName.' '.$spacedictus[$proLingo]["pass"]."<br>";
+} elseif ($subAction == "produce") {
+    $subRating += 1;
+    $subMoney += 1;
+    $subScore += 1;
+    $hashNum = rand(0, 1114111);
+    $hashName = dechex($hashNum);
+    $calcPrice = rand(1, 10);
+    produce($thisParadigm, $yearToday, $sub, $hashName, 'work', $calcPrice);
 }
